f3280b5c951e4377dd58dc36482a5f794a8c286b
RahulNeupane <nrahul503@gmail.com>
2023-02-04 23:39:55 +0545

    completed

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let playerState = 'idle'
+const dropDown = document.getElementById('animations')
+dropDown.addEventListener('change',function(e){
+    playerState = e.target.value
+})
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
-
 canvas.width = 600
 canvas.height = 600
 
@@ -8,18 +12,72 @@ const playerImage = new Image()
 playerImage.src = './shadow_dog.png'
 const spriteWidth = 575
 const spriteheight = 523
-let frameX = 0
-let frameY = 0
 
-
+let gameFrame = 0
+const staggerFrames = 6
+const spriteAnimations = []
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },  
+    {
+        name: 'jump',
+        frames: 7,
+    }, 
+    {
+        name: 'fall',
+        frames: 7,
+    }, 
+    {
+        name: 'run',
+        frames: 9,
+    }, 
+    {
+        name: 'dizzy',
+        frames: 11,
+    }, 
+    {
+        name: 'sit',
+        frames: 5,
+    }, 
+    {
+        name: 'roll',
+        frames: 7,
+    }, 
+    {
+        name: 'bite',
+        frames: 7,
+    }, 
+    {
+        name: 'ko',
+        frames: 12,
+    }, 
+    {
+        name: 'getHit',
+        frames: 4,
+    }, 
+]
+animationStates.forEach((state,index)=>{
+    let frames = {
+        loc: []
+    }
+    for(let j = 0;j< state.frames;j++){
+        let positionX = j * spriteWidth
+        let positionY = index * spriteheight
+        frames.loc.push({x:positionX,y:positionY})
+    }
+    spriteAnimations[state.name] = frames
+})
+console.log(spriteAnimations)
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
-    // ctx.fillRect(100,50,100,100)
-    ctx.drawImage(playerImage,frameX * spriteWidth,frameY * spriteheight,spriteWidth,spriteheight,0,0,spriteWidth,spriteheight)
-    // ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh)
-    // these 9 elements represent
-    // first 4 for source Image 
-    // and last 4 for our canvas i.e. destination 
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length
+    let frameX = position * spriteWidth
+    let frameY = spriteAnimations[playerState].loc[position].y
+    ctx.drawImage(playerImage,frameX,frameY ,spriteWidth,spriteheight,0,0,spriteWidth,spriteheight)
+    
+    gameFrame++
     requestAnimationFrame(animate)
 }
 
